a53b4b5d58212d45ce3195c9063e02f82fe17b33
Joris <reixjoris@gmail.com>
2022-03-22 18:27:18 +0100

    docs: examples

---
+++ examples/src/samples/Tailwind.tsx
@@ -0,0 +1,13 @@
+import { Phone } from 'react-telephone';
+
+export default function Tailwind() {
+  return (
+    <Phone className="relative rounded text-white shadow-sm bg-grey-800 -space-y-px">
+      <Phone.Country className="block bg-transparent p-2 outline-none focus:ring-1 focus:ring-blue-300 focus:border-blue-300 rounded-none rounded-t border border-grey-500/50 text-base" />
+      <Phone.Number
+        placeholder="6 12 34 56 78"
+        className="block bg-transparent px-3 py-2 rounded-none rounded-b border outline-none w-full border-grey-500/50 text-base placeholder:text-grey-500/40 focus:ring-1 focus:ring-blue-300 focus:border-blue-300"
+      />
+    </Phone>
+  );
+}
