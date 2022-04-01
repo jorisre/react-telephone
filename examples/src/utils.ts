d7e95285fd5f3d48056e15446f1f0cb07db966fb
Joris <reixjoris@gmail.com>
2022-04-01 19:14:13 +0200

    refactor: fix lint issue

--- examples/src/utils.ts
+++ examples/src/utils.ts
@@ -10,7 +10,7 @@ export enum Sample {
 export type SampleKey = keyof typeof Sample;
 
 export function getSampleLinks(name: SampleKey) {
-  const GITHUB_REPO_EXAMPLES = `https://github.com/jorisre/react-telephone/tree/main/examples`;
+  const GITHUB_REPO_EXAMPLES = 'https://github.com/jorisre/react-telephone/tree/main/examples';
   const githubSamplePath = `src/components/samples/${name}.tsx`;
 
   return {
