e23b98cb25e1e097636e54cd155f980cb40b916d
Joris <reixjoris@gmail.com>
2022-04-01 22:40:00 +0200

    docs: bump deps

--- examples/src/main.tsx
+++ examples/src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+// Create a root.
+const root = ReactDOM.createRoot(document.getElementById('root') as Element);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
